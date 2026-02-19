b9024d8a118c556a663b83778d00d8161a82c963
Your Name <you@example.com>
2026-02-19 10:47:35 -0300

    contagem

--- entrada.py
+++ entrada.py
@@ -5,12 +5,18 @@ print('Sua senha é:\n',senha)
 dominio =input('Qual seu dominio:\n')
 print('O seu dominio é:\n',dominio)
 
-email = nome + '@' + dominio
-print('Seu email é:\n',email + '.com')
+email = nome + '@' + dominio 
+print('Seu email é:\n',email)
 
 palavra = 'jaca'
 #colocar a string como toda maiuscula
 print('Colocando o texto em maiuscula:',palavra.upper())
 PALAVRA = 'JACA'
 print('Palavra em Minuscula:', PALAVRA.lower())
- 
+
+contagem = email
+
+print('SUA NOVA SENHA É:','a' + str(contagem.count('a')) + 'e' + str(contagem.count('e')) + 'i' + str(contagem.count('i')) + 'o' + str(contagem.count('o')) + 'u' + str(contagem.count('u')))
+
+
+
